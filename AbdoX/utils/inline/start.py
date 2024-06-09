318c07c026dc1e3643b2c87a964646fe6f07a592
Coi SR <150426205+nossah56@users.noreply.github.com>
2024-06-09 19:05:48 +0300

    Add files via upload

---
+++ AbdoX/utils/inline/start.py
@@ -0,0 +1,36 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from AbdoX import app
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url= "https://t.me/huntersource"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        
+        [
+            InlineKeyboardButton(text="مطور السورس ", url=f"https://t.me/U_7h1"),
+            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url=f"https://t.me/huntersource"), 
+        ],
+        [
+            
+            InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=f"https://t.me/l2_2Y") , 
+        ],
+    ]
+    return buttons
